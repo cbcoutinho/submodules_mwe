c26a7a5ed396eb6396d1dc534438296a9312b3a8
cbcoutinho <chrisbcoutinho@gmail.com>
2017-12-14 17:10:30 +0100

    Additional comments

--- example_mod.f90
+++ example_mod.f90
@@ -3,16 +3,16 @@ module example_mod
     ! This module contains two interfaces:
     !   1. f_interface, which is an interface to the local f function
     !   2. g, which is implemented in the example_smod submodule
-    !
-    ! As well as a single local function
 
     interface f_interface
+        ! The function `f` is defined below, within the `contains` statement
         module function f(x) result(y)
             integer :: x, y
         end function f
     end interface f_interface
 
     interface
+        ! The function `g` is implemented in example_smod.f90
         module function g(x) result(y)
             integer :: x,y
         end function g
